914d58ad7c25677226044eb4f1e9d2e0e475de14
Taggartjoey9 <taggartjoey9@gmail.com>
2025-01-21 08:32:47 -0500

    connecting user inputs to form submit

--- ticket-system/src/App.jsx
+++ ticket-system/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   <div className="App">
       <div className="container">
         <h1>Ticketing System</h1>
+        <TicketForm/>
       </div>
   </div>
   )
--- ticket-system/src/components/TicketForm.jsx
+++ ticket-system/src/components/TicketForm.jsx
@@ -1,7 +1,39 @@
 import { useState } from "react"
 
+
 export default function TicketForm() {
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [priority, setPriority] = useState(1);
+
+    const priorityLevels = {
+        1: "Low",
+        2: "Medium",
+        3: "High"
+    }
+
+        const clearForm = () => {
+            setTitle('');
+            setDescription('');
+            setPriority(1);
+        }
+
+        function handleSubmit(e) {
+            e.preventDefault();
+            clearForm();
+        }
+
     return (
-        <></>
+        <form onSubmit={handleSubmit} className="ticket-form">
+            <div>
+                <label>Title</label>
+                <input type="text" className="form-input" value={title} onChange={(e) => setTitle(e.target.value)} />
+            </div>
+            <div>
+                <label>Description</label>
+                <textarea type="text" className="form-input" value={description} onChange={(e) => setDescription(e.target.value)} />
+            </div>
+        </form>
     )
 }
