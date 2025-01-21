b084575aca2bbe510a1f9764df46e744fd3384eb
Taggartjoey9 <taggartjoey9@gmail.com>
2025-01-20 21:40:09 -0500

    ticketform setup

--- ticket-system/src/App.jsx
+++ ticket-system/src/App.jsx
@@ -1,11 +1,13 @@
-
+import TicketForm from "./components/TicketForm"
 
 function App() {
 
   return (
-    <>
-  
-    </>
+  <div className="App">
+      <div className="container">
+        <h1>Ticketing System</h1>
+      </div>
+  </div>
   )
 }
 
