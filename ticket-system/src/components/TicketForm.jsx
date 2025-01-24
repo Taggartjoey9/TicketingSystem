eeeca52f93c34f572b33ca7c493a98214c047eec
Taggartjoey9 <taggartjoey9@gmail.com>
2025-01-23 19:32:40 -0500

    added priority btns and watch users updates when submitting tickets

--- ticket-system/src/components/TicketForm.jsx
+++ ticket-system/src/components/TicketForm.jsx
@@ -5,7 +5,7 @@ export default function TicketForm() {
 
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
-    const [priority, setPriority] = useState(1);
+    const [priority, setPriority] = useState("1");
 
     const priorityLevels = {
         1: "Low",
@@ -16,11 +16,21 @@ export default function TicketForm() {
         const clearForm = () => {
             setTitle('');
             setDescription('');
-            setPriority(1);
+            setPriority("1");
         }
 
         function handleSubmit(e) {
             e.preventDefault();
+
+            const newSubmit = {
+                id: new Date().toISOString(),
+                title,
+                description,
+                priority
+            }
+
+            console.log(newSubmit)
+
             clearForm();
         }
 
@@ -34,6 +44,19 @@ export default function TicketForm() {
                 <label>Description</label>
                 <textarea type="text" className="form-input" value={description} onChange={(e) => setDescription(e.target.value)} />
             </div>
+
+        <fieldset className="priority-fieldset">
+            <legend>Priority</legend>
+            {
+                Object.entries(priorityLevels).map(([value, label]) => (
+                    <label key={value} className="priority-label">
+                        <input type="radio" className="priority-input" value={value} checked={priority === value} onChange={(e) => setPriority(e.target.value)}/>
+                        {label}
+                    </label>
+                ))
+            }
+        </fieldset>
+            <button type="submit" className="button">Submit</button>
         </form>
     )
 }
